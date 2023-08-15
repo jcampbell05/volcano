16bc196398ea7687ba861c9fad3e3e334941122c
james <james@jamess-MacBook-Pro.local>
2023-08-15 10:06:56 +0100

    Import vols

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='volcano',
     version='0.1',
     package_data={
-        'volcano': ['image.png'],
+        'volcano': ['*.vol'],
     },
     entry_points='''
         [console_scripts]
