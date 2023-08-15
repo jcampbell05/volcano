05b537ce9df5c160462ab417601b47987981ca00
james <james@jamess-MacBook-Pro.local>
2023-08-15 18:06:28 +0100

    REmove todo

--- volcano/compiler.py
+++ volcano/compiler.py
@@ -23,8 +23,6 @@ class VolcanoTransformer(ast.NodeTransformer):
 
 class VolcanoVisitor(ast.NodeVisitor):
 
-    symbol_prefix = uuid.uuid4()
-
     control_flow_target = False
     capture_call = False
     function_def_has_return = False
