fe7db8092862e22c41499c92edb27709982055eb
james <james@jamess-MacBook-Pro.local>
2023-08-21 14:44:24 +0100

    more advanced system

--- examples/02-python-script/main.py
+++ examples/02-python-script/main.py
@@ -3,6 +3,16 @@ import volcano
 @volcano.volcano
 def my_script():
     from volcano.shell import ls
-    print(ls())
+    
+    print('The files in the current directory are:\n')
+
+    files = ls()
+    count = 0
+
+    for file in files:
+        print(f"- {file}")
+        count += 1
+
+    print(f"\nTotal: {count} files")
 
 my_script()
